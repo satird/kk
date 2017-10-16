5aafd4527a13a8bcc68125951c4ac79d454e8770
satird <satirdima@gmail.com>
2017-10-16 11:11:02 +0300

    нижняя карусель

--- src/js/js.js
+++ src/js/js.js
@@ -1,11 +1,4 @@
-$(document).ready(function(){
- $('.one-time').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-});
-});
-
+$(function() {
+                $('#dg-container').gallery();
+            });
 
