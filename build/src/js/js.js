11cd88af5c4599290b5b69b39f06524d1cbfa6fa
satird <satirdima@gmail.com>
2017-10-18 17:43:45 +0300

    поменял первый слайдер

--- build/src/js/js.js
+++ build/src/js/js.js
@@ -1,4 +1,31 @@
+//скрипт на слайдер в центре
+
 $(function() {
                 $('#dg-container').gallery();
             });
 
+//скрипт на кнопку вверх
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
